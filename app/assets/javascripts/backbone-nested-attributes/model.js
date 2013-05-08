e446f6e927ef60959552acd664def3594bfd0659
Vicente Mundim <vicente.mundim@gmail.com>
2013-05-08 20:02:00 -0300

    Creating nested collections when setting attributes on a model

--- app/assets/javascripts/backbone-nested-attributes/model.js
+++ app/assets/javascripts/backbone-nested-attributes/model.js
@@ -1,5 +1,56 @@
 (function(){
-  Backbone.NestedAttributesModel = Backbone.Model.extend({
+  var BackboneModelPrototype = Backbone.Model.prototype
 
+  Backbone.NestedAttributesModel = Backbone.Model.extend({
+    set: function (key, value, options) {
+      var attributes
+
+      // Duplicate backbone's behavior to allow separate key/value parameters,
+      // instead of a single 'attributes' object.
+      if (_.isObject(key) || key == null) {
+        attributes = key
+        options = value
+      } else {
+        attributes = {}
+        attributes[key] = value
+      }
+
+      if (!attributes) {
+        return this
+      }
+
+      _(this.relations).each(function (relation) {
+        var key   = relation.key,
+            value = attributes[key],
+            currentValue,
+            collection
+
+        if (value) {
+          currentValue = this.get(key)
+
+          if (currentValue) {
+            collection = currentValue
+            collection.set(value)
+          } else {
+            collection = this._createNestedAttributeCollection(relation)
+            collection.add(value)
+          }
+
+          attributes[key] = collection
+        } else {
+          attributes[key] = this._createNestedAttributeCollection(relation)
+        }
+
+      }, this)
+
+      return BackboneModelPrototype.set.call(this, attributes, options)
+    },
+
+    _createNestedAttributeCollection: function (relation) {
+      var collection = new Backbone.Collection
+      collection.model = _(relation).result('relatedModel')
+
+      return collection
+    }
   })
 })()
--- spec/javascripts/backbone-nested-attributes/ModelSpec.js
+++ spec/javascripts/backbone-nested-attributes/ModelSpec.js
@@ -1,5 +1,5 @@
 describe("Backbone.NestedAttributesModel", function() {
-  var model
+  var model, Post, post, Comment, comment
 
   beforeEach(function() {
     model = new Backbone.NestedAttributesModel({})
@@ -7,5 +7,76 @@ describe("Backbone.NestedAttributesModel", function() {
 
   it("should be a Backbone.Model", function() {
     expect(model).toBeAnInstanceOf(Backbone.Model)
-  });
+  })
+
+  describe("with a has many relationship", function() {
+    beforeEach(function() {
+      Post = Backbone.NestedAttributesModel.extend({
+        relations: [
+          {
+            type: 'many',
+            key:  'comments',
+            relatedModel: function () { return Comment }
+          }
+        ]
+      })
+
+      Comment = Backbone.NestedAttributesModel.extend({})
+
+      model = new Post
+    })
+
+    describe("when creating", function() {
+      it("initializes the comments attribute with an empty collection", function() {
+        expect(model.get('comments')).toBeDefined()
+        expect(model.get('comments')).toBeAnInstanceOf(Backbone.Collection)
+      });
+
+      describe("while setting attributes", function() {
+        beforeEach(function() {
+          model = new Post({ foo: 'bar', comments: [{ body: 'some comment'} ] })
+        })
+
+        it("sets the normal attributes", function() {
+          expect(model.get('foo')).toEqual('bar')
+        })
+
+        it("creates the comment inside comments collection", function() {
+          comment = model.get('comments').at(0)
+
+          expect(comment).toBeAnInstanceOf(Comment)
+          expect(comment.get('body')).toEqual('some comment')
+        })
+      })
+    })
+
+    describe("when updating", function() {
+      it("creates the comment inside comments collection", function() {
+        model.set({ comments: [{ body: 'some comment' }] })
+
+        comment = model.get('comments').at(0)
+
+        expect(comment).toBeAnInstanceOf(Comment)
+        expect(comment.get('body')).toEqual('some comment')
+      })
+
+      it("does not creates a new collection, but updates it instead", function() {
+        var originalCollection = model.get('comments')
+
+        model.set({ comments: [{ body: 'some comment' }] })
+        expect(model.get('comments')).toBe(originalCollection)
+      })
+
+      it("updates existing models in the collection", function() {
+        var existingComment
+
+        model.set({ comments: [{ id: '123', body: 'some comment' }] })
+        existingComment = model.get('comments').at(0)
+
+        model.set({ comments: [{ id: existingComment.id, body: 'some other comment' }] })
+
+        expect(model.get('comments').at(0).get('body')).toEqual('some other comment')
+      })
+    })
+  })
 })
